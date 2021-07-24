1191ddd9173aba7602e786056c8aeb4d89a2f9f4
Slim-Ild <ttifo@bk.ru>
2021-07-24 18:25:42 +0300

    Добавьте файлы проекта.

---
+++ Ild-PictureDetector/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using AForge.Video.DirectShow;
+using Ild_PictureRecognizer_lib.Detectors;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Ild_PictureDetector
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string path;
+        private string url;
+
+        private FilterInfoCollection collection;
+        private VideoCaptureDevice device;
+
+        private IEnumerable<string> labelssDetectedEntities = new string[] { "Faces", "Eyes", "Full Body", "Upper Body", "Lower Body", "Licences", "Licences_RUS"};
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbDetect.IsEnabled = false;
+            btnDetect.IsEnabled = false;
+            imgDetected.IsEnabled = false;
+            imgDetectedGray.IsEnabled = false;
+            btnCamera.IsEnabled = false;
+            imgMain.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "/Image Sources/wait_image.png"));
+            imgDetected.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "/Image Sources/detector.png"));
+            imgDetectedGray.Source = new BitmapImage(new Uri(Environment.CurrentDirectory + "/Image Sources/detector.png"));
+            cmbDetect.ItemsSource = this.labelssDetectedEntities;
+
+        }
+
+        private void btnDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog();
+
+                if (dialog.ShowDialog() == true) 
+                {
+                    imgMain.Source = new BitmapImage(new Uri(dialog.FileName));
+                    this.path = dialog.FileName;
+                    cmbDetect.IsEnabled = true;
+                    btnDetect.IsEnabled = true;
+                    imgDetected.IsEnabled = true;
+                    imgDetectedGray.IsEnabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnDetect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FaceDetector detector;
+                FaceDetector.detectedEntities entities = FaceDetector.detectedEntities.Face;
+
+                switch (cmbDetect.SelectedIndex)
+                {
+                    case 0:
+                        entities = FaceDetector.detectedEntities.Face;
+                        break;
+                    case 1:
+                        entities = FaceDetector.detectedEntities.Eyes;
+                        break;
+                    case 2:
+                        entities = FaceDetector.detectedEntities.FullBody;
+                        break;
+                    case 3:
+                        entities = FaceDetector.detectedEntities.UpperBody;
+                        break;
+                    case 4:
+                        entities = FaceDetector.detectedEntities.LowerBody;
+                        break;
+                    case 5:
+                        entities = FaceDetector.detectedEntities.LicensePlate;
+                        break;
+                    case 6:
+                        entities = FaceDetector.detectedEntities.LicensePlate_RUS;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (path != null)
+                {
+                    var snapImage = new Bitmap(this.path);
+                    detector = new FaceDetector(snapImage);
+                }
+                else
+                {
+                    detector = new FaceDetector(new FaceDetector().ConvertFromSource((BitmapImage)imgMain.Source));
+                }
+
+                detector.PrepareCascade(entities);
+                detector.ProccessPicture();
+                imgDetected.Source = detector.GetResult();
+                imgDetectedGray.Source = detector.GetGrayResult();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnCamera_Click(object sender, RoutedEventArgs e)
+        {
+            device = new VideoCaptureDevice(collection[lsCams.SelectedIndex].MonikerString);
+            device.NewFrame += Device_NewFrame;
+            device.Start();
+        }
+
+        private void Device_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            try
+            {
+                var image = (Bitmap)eventArgs.Frame.Clone();
+                var frame = new FaceDetector().ConvertToSource(image);
+                Dispatcher.BeginInvoke(new ThreadStart(delegate
+                {
+                    imgMain.Source = frame;
+                }));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            collection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo item in collection)
+            {
+                lsCams.Items.Add(item.Name.ToString());
+            }
+        }
+
+        private void lsCams_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            cmbDetect.IsEnabled = true;
+            btnDetect.IsEnabled = true;
+            imgDetected.IsEnabled = true;
+            imgDetectedGray.IsEnabled = true;
+            btnCamera.IsEnabled = true;
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var saveFile = new SaveFileDialog();
+            FileStream fileStream = null;
+            Bitmap saveImage = new FaceDetector().ConvertFromSource((BitmapImage)imgDetected.Source);
+
+            if (saveFile.ShowDialog() == true)
+            {
+                saveFile.FileName = saveFile.FileName + ".Jpeg";
+                fileStream = (FileStream)saveFile.OpenFile();
+                saveImage.Save(fileStream, ImageFormat.Jpeg);
+                fileStream.Close();
+            }
+        }
+
+        private void btnVideoDirectory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var dialog = new OpenFileDialog();
+
+                if (dialog.ShowDialog() == true)
+                {
+                    imgMain.Source = new BitmapImage(new Uri(dialog.FileName));
+                    this.path = dialog.FileName;
+                    cmbDetect.IsEnabled = true;
+                    btnDetect.IsEnabled = true;
+                    imgDetected.IsEnabled = true;
+                    imgDetectedGray.IsEnabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
---
+++ Ild-PictureRecognizer_lib/Detectors/FaceDetector.cs
@@ -0,0 +1,121 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System;
+
+namespace Ild_PictureRecognizer_lib.Detectors
+{
+    public class FaceDetector
+    {
+        public enum detectedEntities { Face, Eyes, FullBody, UpperBody, LowerBody, LicensePlate, LicensePlate_RUS}
+
+        Image<Bgr, byte> image = null;
+        Image<Gray, byte> grayImage = null;
+        Bitmap outpuImage = null;
+        Bitmap outputGrayImage = null;
+        CascadeClassifier cascade = null;
+
+        public FaceDetector()
+        {
+
+        }
+
+        public FaceDetector(Bitmap inputImage)
+        {
+            this.image = new Image<Bgr, byte>(inputImage);
+            grayImage = image.Convert<Gray, byte>();
+            CvInvoke.EqualizeHist(grayImage,grayImage);
+        }
+
+        public void PrepareCascade(detectedEntities entity)
+        {
+            switch (entity)
+            {
+                case detectedEntities.Face:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_frontalface_alt.xml");
+                    break;
+                case detectedEntities.Eyes:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_eye.xml");
+                    break;
+                case detectedEntities.FullBody:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_fullbody.xml");
+                    break;
+                case detectedEntities.UpperBody:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_upperbody.xml");
+                    break;
+                case detectedEntities.LowerBody:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_lowerbody.xml");
+                    break;
+                case detectedEntities.LicensePlate:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_licence_plate_rus_16stages.xml");
+                    break;
+                case detectedEntities.LicensePlate_RUS:
+                    this.cascade = new CascadeClassifier(Environment.CurrentDirectory + @"\Resources\haarcascade_russian_plate_number.xml");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void ProccessPicture()
+        {
+            Rectangle[] faces = cascade.DetectMultiScale(grayImage, 1.1, 3, Size.Empty, Size.Empty);
+
+            foreach (Rectangle face in faces) 
+            {
+                CvInvoke.Rectangle(image, face, new MCvScalar(02, 0, 255), 3, Emgu.CV.CvEnum.LineType.Filled);
+
+                Image<Bgr, byte> resultImage = image.Convert<Bgr, byte>();
+                resultImage.ROI = face;
+                resultImage.Resize(158,158, Inter.Cubic);
+                outpuImage = resultImage.ToBitmap();
+                outputGrayImage = resultImage.Convert<Gray, byte>().Bitmap;
+            }
+        }
+
+        public BitmapImage ConvertToSource(Bitmap img)
+        {
+            using (var memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+
+                return bitmapImage;
+            }
+        }
+
+        public Bitmap ConvertFromSource(BitmapImage bitmapImage)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                BitmapEncoder enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(bitmapImage));
+                enc.Save(outStream);
+                Bitmap bitmap = new Bitmap(outStream);
+
+                return new Bitmap(bitmap);
+            }
+        }
+
+        public BitmapImage GetResult() 
+        {
+            return ConvertToSource(outpuImage);
+        }
+
+        public BitmapImage GetGrayResult() 
+        {
+            return ConvertToSource(outputGrayImage);
+        }
+    }
+}
